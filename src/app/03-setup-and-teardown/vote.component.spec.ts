1890b8e9dd5174d489da5ca683d85b9a2c5cb849
ChrisLubin <chrisx700@gmail.com>
2020-11-04 22:06:28 -0500

    Created unit tests for vote component

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,26 @@
 import { VoteComponent } from './vote.component';
 
-xdescribe('VoteComponent', () => {
-  it('', () => {
+describe('VoteComponent', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    // Set up
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment total votes when upvoted', () => {
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement total votes when downvoted', () => {
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
